f38c8fad54fb9b2748214e11a8e4fe926868f1f4
Karabo <ktouching@gmail.com>
2026-03-16 15:54:19 +0200

    -

--- routes/api.php
+++ routes/api.php
@@ -98,6 +98,11 @@ Route::post('/login', function (Request $request) {
             'phone'     => $user->phone,
             'occupation'=> $user->occupation,
             'role'      => $user->role,
+            'address_line_1' => $user->address_line_1,
+            'suburb' => $user->suburb,
+            'longitude' => $user->longitude,
+            'latitude' => $user->latitude,
+            'complex' => $user->complex
         ],
         'channels' => $channels,
         'token'    => $token,
