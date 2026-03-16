c198b9a155014aa37efa266d7a46b0505fd3f36d
Karabo <ktouching@gmail.com>
2026-03-16 02:39:03 +0200

    -

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -36,9 +36,5 @@ class Employee extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
-
-     public function resolveRouteBinding($value, $field = null)
-    {
-        return $this->where('user_id', $value)->firstOrFail();
-    }
+    
 }
