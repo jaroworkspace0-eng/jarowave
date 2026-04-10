917c8f3a985bbb184f859539eac6f0cf647eba83
Karabo <ktouching@gmail.com>
2026-04-10 04:11:28 +0200

    -

--- resources/js/pages/employees/index.vue
+++ resources/js/pages/employees/index.vue
@@ -890,7 +890,7 @@ const hideSuggestions = () => {
                 <!-- INVITE LINKS CARD -->
                 <div
                     class="mb-6 rounded-xl border border-orange-200 bg-orange-50 p-5"
-                    style="display: none"
+                    style="display: block"
                 >
                     <div class="mb-4 flex items-center justify-between">
                         <div>
