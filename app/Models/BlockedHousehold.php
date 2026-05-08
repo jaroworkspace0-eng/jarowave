5f48b1541a3aa99d1d6dac08965e647283c57631
Karabo <ktouching@gmail.com>
2026-05-08 10:33:23 +0200

    -

--- app/Models/BlockedHousehold.php
+++ app/Models/BlockedHousehold.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BlockedHousehold extends Model
 {
     protected $fillable = ['user_id', 'blocked_user_id'];
+
+    public function blockedUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'blocked_user_id');
+    }
+
+    public function blocker(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
