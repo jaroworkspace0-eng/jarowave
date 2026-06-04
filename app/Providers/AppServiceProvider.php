bbdeb12585b7265a28fd7a7eb395e06908c5214a
Karabo <ktouching@gmail.com>
2026-06-04 13:34:00 +0200

    -

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,18 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        View::addNamespace('mail', resource_path('views/vendor/mail/html'));
     }
 }
