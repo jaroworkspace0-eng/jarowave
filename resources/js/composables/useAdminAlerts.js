fbb49cd4ddadb134695ace84e08fa77b96898d3e
Karabo <ktouching@gmail.com>
2026-08-01 21:42:47 +0200

    -

--- resources/js/composables/useAdminAlerts.js
+++ resources/js/composables/useAdminAlerts.js
@@ -102,6 +102,8 @@ export function useAdminAlerts() {
     });
 
     socket.on('alert:new', (alert) => {
+        console.log('[alert:new] received:', alert);
+        console.log('[alert:new] channelGuards:', alert.channelGuards);
         const isLiveArrival = hydrated.value;
         const existingKey = findAlertKey(alerts, alert.id);
         alerts.set(existingKey !== undefined ? existingKey : alert.id, {
