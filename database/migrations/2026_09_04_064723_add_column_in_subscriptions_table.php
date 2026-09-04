00962aba40f6d694be7e45dcb6d3bf9b52931c53
Karabo <ktouching@gmail.com>
2026-09-04 06:49:32 +0200

    -

---
+++ database/migrations/2026_09_04_064723_add_column_in_subscriptions_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->index(
+                ['cancellation_reason', 'status', 'channel_subscription_id'],
+                'subscriptions_optin_lookup_index'
+            );
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->dropIndex('subscriptions_optin_lookup_index');
+        });
+    }
+};
